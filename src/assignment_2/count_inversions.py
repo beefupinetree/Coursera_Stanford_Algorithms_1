527058d3b8586e4a71a9dcab853f285f38850bbc
Tarek Samhan <tsamhan@gmail.com>
2025-01-03 11:53:28 -0500

    Minor changes

--- src/assignment_2/count_inversions.py
+++ src/assignment_2/count_inversions.py
@@ -3,15 +3,19 @@
 
 
 def inversions(arr: list[int]):
+    print(f"arr is {arr}")
     if len(arr) <= 1:
         return arr, 0
     mid = len(arr) // 2
 
     p = arr[0:mid]
     a, inversion_p = inversions(p)
+    print(f"a is {a}")
 
     q = arr[mid:]
     b, inversions_q = inversions(q)
+    print(f"a is {a}, b is {b}")
+    # a and b are sorted arrays at this point
     c, cross_inversions = _count_cross_inversions(a, b)
 
     num_inversions = inversion_p + inversions_q + cross_inversions
@@ -20,12 +24,14 @@ def inversions(arr: list[int]):
 
 def _count_cross_inversions(p: list[int], q: list[int]):
     r = []
-    i = j = num_inversion = 0
+    print(f"p is {p}, q is {q}")
+    i = 0
+    j = 0
+    num_inversion = 0
     while i < len(p) and j < len(q):
         if p[i] > q[j]:
             # if P[1] > Q[j], then P[k] > Q[k] for all  i < k <= len(P)
-            # These are all inversions. The claim emerges from the
-            # property that P is sorted.
+            # These are all inversions. It assumes that 'p' is sorted.
             num_inversion += len(p) - i
             r.append(q[j])
             j += 1
@@ -39,3 +45,7 @@ def _count_cross_inversions(p: list[int], q: list[int]):
         r.extend(q[j:])
 
     return r, num_inversion
+
+
+if __name__ == "__main__":
+    inversions([3, 2, 1, 5])
